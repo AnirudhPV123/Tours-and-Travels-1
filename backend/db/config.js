2a57fb92e920fa0a0f36b57b76149992d9cd5075
AnirudhPV123 <pvanirudh96@gmail.com>
2023-07-20 21:26:31 +0530

    new update 5

--- backend/db/config.js
+++ backend/db/config.js
@@ -2,7 +2,6 @@
 const { MongoClient } = require('mongodb')
 
 const url = process.env.MONGODB_URI
-// const databaseName='tours_and_travels'
 
 const client = new MongoClient(url)
 
@@ -10,7 +9,6 @@ const client = new MongoClient(url)
 async function dbConnect() {
 
     let result = await client.connect();
-    // return result.db()
     return result.db()
 }
 
