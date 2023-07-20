9574548896f28fc194dca56466b93f40286858a7
AnirudhPV123 <pvanirudh96@gmail.com>
2023-07-20 21:03:07 +0530

    new update1

--- frontend/src/Url.js
+++ frontend/src/Url.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-// export const backend_url = "https://tours-and-travels-fh5b.onrender.com"
-
-export const backend_url = "http://localhost:5001"
-=======
 export const backend_url = "https://tours-and-travels-fh5b.onrender.com"
->>>>>>> cbe2d55023898dcf6a26020a7d369f3f1ecff5ec
+
